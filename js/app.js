3dc825d0ed539cb78a06aeb68ffbefc36d3bc098
jenny-berggreen <jenny@berggreen.no>
2024-02-15 17:07:43 +0100

    Register medicines with and without prescription and log them

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 const medicines = [];
+const prescriptionMedicines = [];
 
 // ----- GET ELEMENTS FROM THE DOM -----
+const nameInput = document.querySelector('.name');
+const idInput = document.querySelector('.id');
+const manufacturerInput = document.querySelector('.manufacturer');
 const quantitySelect = document.querySelector('.quantity');
+
+const radioButtons = document.querySelectorAll('.prescription-radio-button');
 const ageSelect = document.querySelector('.age');
 const refillsSelect = document.querySelector('.refills');
 
-const radioButtons = document.querySelectorAll('.prescription-radio-button');
+const submitButton = document.querySelector('.submit-button');
+
 
 // ----- INSERT OPTIONS IN SELECTS -----
 // quantity
@@ -35,7 +42,7 @@ for (let i = 0; i <= 10; i++) {
 // ----- EVENT LISTENERS -----
 radioButtons.forEach(button => {
 	button.addEventListener('change', ()=> {
-		if (button.id === 'radio-no') {
+		if (button.value === 'no') {
 			refillsSelect.setAttribute('disabled', '');
 			ageSelect.removeAttribute('disabled');
 		} else {
@@ -44,6 +51,30 @@ radioButtons.forEach(button => {
 		}
 	})
 });
+
+submitButton.addEventListener('click', (e)=> {
+	e.preventDefault();
+
+	let newMedicine;
+
+	// loop through radio buttons to find the selected one
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            if (radioButtons[i].value === 'no') {
+                newMedicine = new Medicine(nameInput.value, idInput.value, manufacturerInput.value, quantitySelect.value, radioButtons[i].value, ageSelect.value);
+            } else {
+                newMedicine = new PrescriptionMedicine(nameInput.value, idInput.value, manufacturerInput.value, quantitySelect.value, radioButtons[i].value, refillsSelect.value);
+            }
+            // break out of the loop once the selected radio button is found
+            break;
+        }
+    }
+
+	Medicine.addMedicine(newMedicine);
+	console.log(newMedicine);
+	console.log(medicines);
+	console.log(prescriptionMedicines);
+})
 
 // ----- DECLARING MEDICINE CLASS -----
 class Medicine {
@@ -58,7 +89,11 @@ class Medicine {
 
 	// add method
 	static addMedicine(medicine) {
-		medicines.push(medicine);
+		if (medicine.prescription === 'no') {
+			medicines.push(medicine);
+		} else {
+			prescriptionMedicines.push(medicine);
+		}
 	}
 }
 
